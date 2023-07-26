0cb3ad6b9cf88ce90dbe6c092a8babadcc57d07e
Lawrence Nigel Anderson <nigel.lawrenceanderson@gmail.com>
2023-07-26 16:29:25 -0500

    completes lab

--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,23 @@
 #!/usr/bin/env python3
 
 class Shoe:
+    
+    def __init__(self, brand, size, condition="old"):
+        self.brand = brand
+        self.size = size
+        self.condition = condition
+    def cobble(self):
+        cobble ="Your shoe is as good as new!"
+        print(cobble)
+        self.condition = "New"
+    def get_size(self):
+        return self._size
+    
+    def set_size(self, size):
+        if type(size) == int:
+            self._size = size
+        else:
+            print("size must be an integer")
+
+    size = property(get_size, set_size)
     pass
